bc5911ad7c78f46b9e9eeb68a377fc3cefb0f217
meganlechner <meganlechner@gmail.com>
2021-03-18 20:41:36 -0500

    Part 2, Controllers.5, index not working

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,25 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank(message = "Skill set cannot be blank")
+    private String skill;
+
+    public Skill(@NotBlank(message = "Skill set cannot be blank") String skill) {
+        this.skill = skill;
+    }
+
+    public Skill() {};
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
 }
